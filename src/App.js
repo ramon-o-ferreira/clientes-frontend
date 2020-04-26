8f4bb19ce28e82bdde66f2f237d1b407d7ca595f
ramon-o-ferreira <ramon-o-ferreira@users.noreply.github.com>
2020-04-26 00:32:48 -0300

    First commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+
+import './global.css'
+
+import Routes from './routes'
+
+// JSX (JavaScript XML) HTML dentro do JS
+
+function App() {
+  return (
+    <Routes />
+  );
+}
+
+export default App;
